7c44b41071ef43f6f52ed323fc5053611c49685b
alphaseinor <alphaseinor@gmail.com>
2021-04-26 00:09:43 -0500

    added endpoint default returns, cors and helmet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const helmet = require('helmet');
+const cors = require('cors');
 const path = require('path');
 const Routes = require('./routes');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json());
+app.use(helmet());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../README.md')));
 
